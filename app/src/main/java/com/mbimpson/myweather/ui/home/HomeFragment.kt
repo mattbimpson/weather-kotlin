3ea06930c3fdb1d26d554ea656d62131558a82e2
Matt Bimpson <m.bimpson@gmail.com>
2020-02-11 20:01:34 -0500

    tidy up + add new icon

--- app/src/main/java/com/mbimpson/myweather/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mbimpson/myweather/ui/home/HomeFragment.kt
@@ -7,11 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mbimpson.myweather.GlobalVariables
 import com.mbimpson.myweather.R
 import java.net.URL
@@ -19,7 +15,6 @@ import org.json.JSONObject
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
     private var City: String = "Bogota"
 
     override fun onCreateView(
@@ -27,15 +22,11 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val titleTextView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            val sharedPrefs = activity?.getPreferences(Context.MODE_PRIVATE)
-            val city = sharedPrefs?.getString(getString(R.string.prefs_city_key), null)
-            titleTextView.text = city
-        })
+        val sharedPrefs = activity?.getPreferences(Context.MODE_PRIVATE)
+        val city = sharedPrefs?.getString(getString(R.string.prefs_city_key), null)
+        titleTextView.text = city
 
         weatherTask().execute()
 
--- app/src/main/java/com/mbimpson/myweather/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/mbimpson/myweather/ui/settings/SettingsFragment.kt
@@ -8,29 +8,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.mbimpson.myweather.R
 
 
 class SettingsFragment : Fragment() {
-
-    private lateinit var settingsViewModel: SettingsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        settingsViewModel =
-            ViewModelProviders.of(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.text_settings)
-        settingsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
 
         val editCity: EditText = root.findViewById(R.id.edit_city)
         editCity.addTextChangedListener(object : TextWatcher {
